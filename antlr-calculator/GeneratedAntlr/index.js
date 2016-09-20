8d53ea7f3b7b73f3d94952449ceaa7807ea9a77a
Georg Dangl <georg@georgdangl.de>
2016-09-20 20:54:11 +0200

    Add typings for antlr4 and the generated lexer, parser and visitor

---
+++ antlr-calculator/GeneratedAntlr/index.js
@@ -0,0 +1,3 @@
+﻿exports.CalculatorLexer = require('./CalculatorLexer.js').CalculatorLexer;
+exports.CalculatorParser = require('./CalculatorParser.js').CalculatorParser;
+exports.CalculatorVisitor = require('./CalculatorVisitor.js').CalculatorVisitor;
