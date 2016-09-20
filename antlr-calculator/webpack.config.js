14b27d4e28f71a06e5181aa835398498b3740c4d
Georg Dangl <georg@georgdangl.de>
2016-09-20 21:20:49 +0200

    Add webpack.config and generated bundle.js

---
+++ antlr-calculator/webpack.config.js
@@ -0,0 +1,32 @@
+﻿var webpack = require('webpack');
+
+module.exports = {
+    entry: "./Calculator",
+    output: {
+        filename: "./dist/bundle.js",
+        library: ["antlrCalc"] // Global variable name
+    },
+    devtool: "source-map",
+    node: {
+        fs: "empty"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: ['ts']
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ],
+    resolve: {
+        extensions: ['', '.ts', '.js']
+    }
+};
