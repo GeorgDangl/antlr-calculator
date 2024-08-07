bd8403c9c74a1f7f4ce3a58132774cdc86aa8c03
ruslan-minukov <ruslan.minukov@qit.software>
2024-08-07 11:51:14 +0300

    feat: updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,45 @@
-﻿const path = require('path')
+﻿const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        'antlrCalc': './src/index.ts'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist', 'bundles'),
-        filename: '[name].js',
-        libraryTarget: 'umd',
-        library: 'antlrCalc',
-        umdNamedDefine: true
-    },
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    module: {
-        rules: [{
-            test: /\.ts$/,
-            loaders: ['ts-loader'],
-            exclude: /node_modules/
-        }]
-    },
-    optimization: {
-        minimizer: [
-          new TerserPlugin({
-            cache: true,
-            parallel: true,
-            sourceMap: true
-          }),
-        ],
-      },
-      performance: {
-        maxEntrypointSize: 512000,
-          maxAssetSize: 512000
-      }
+  entry: {
+    'antlrCalc': './src/index.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, 'demo', 'dist'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: 'antlrCalc',
+    umdNamedDefine: true
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [{
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_DEBUG': false,
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+    ],
+  },
+  performance: {
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  }
 };
